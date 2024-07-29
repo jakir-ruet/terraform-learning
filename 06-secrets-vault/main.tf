6654ebd74cc27624893bb7157545acb1539d6cde
jakir-ruet <jakir.ruet.bd@gmail.com>
2024-07-29 19:09:18 +0600

    vault lab ongoing

--- 06-secrets-vault/main.tf
+++ 06-secrets-vault/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 provider "vault" {
-  address = "<>:80"
+  address = "18.221.82.255:80"
   skip_child_token = true
   auth_login {
     path = "auth/approle/login"
     parameters = {
-      role_id = "<>"
-      secret_id = "<>"
+      role_id = "743f0h42-8d64-nf5e-9834-j0fgmbvsmv"
+      secret_id = "gf3f042-8fgd4-jgte-7843-h843dhsdr"
     }
   }
 }
