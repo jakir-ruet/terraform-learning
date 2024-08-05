ea73f7997dab52c5689b2db68cad3af4e8fd61ec
jakir-ruet <jakir.ruet.bd@gmail.com>
2024-08-05 21:08:01 +0600

    ip updated

--- 17-mock-test/01-vpc-design/03-use-terraform-std/04-vpc-variables.tf
+++ 17-mock-test/01-vpc-design/03-use-terraform-std/04-vpc-variables.tf
@@ -24,21 +24,21 @@ variable "vpc_availability_zones" {
 variable "vpc_public_subnets" {
   description = "VPC Public Subnets"
   type = list(string)
-  default = [ "10.0.101.0/24", "10.0.102.0/24" ]
+  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
 }
 
 # VPC Private Subnets
 variable "vpc_private_subnets" {
   description = "VPC Private Subnets"
   type = list(string)
-  default = [ "10.0.1.0/24", "10.0.2.0/24" ]
+  default = [ "10.0.3.0/24", "10.0.4.0/24" ]
 }
 
 # VPC Database Subnets
 variable "vpc_database_subnets" {
   description = "VPC Database Subnets"
   type = list(string)
-  default = [ "10.0.151.0/24", "10.0.152.0/24" ]
+  default = [ "10.0.6.0/24", "10.0.7.0/24" ]
 }
 
 # Database Subnet Group (True/False) for VPC
